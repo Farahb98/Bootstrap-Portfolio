1a5a0f3e93e03bcbd8ee5e67fcbd75c079d2457e
unknown <farahboujana2@gmail.com>
2023-09-23 20:20:47 +0100

    added map and contact details

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//map
+
+var map = L.map("map").setView([52.48127656873457, -1.8387653886166546], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
